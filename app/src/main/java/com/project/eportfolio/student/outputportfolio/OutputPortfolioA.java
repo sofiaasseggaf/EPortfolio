03f895db5ac2afb8bf09ca8cb54948d27f7a8adf
sofiaasseggaf <sofiaasseggaf@gmail.com>
2021-04-23 20:59:13 +0700

    MCCafe 20.57
    sisa onclicklistener, swipe, detail

---
+++ app/src/main/java/com/project/eportfolio/student/outputportfolio/OutputPortfolioA.java
@@ -0,0 +1,18 @@
+package com.project.eportfolio.student.outputportfolio;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.eportfolio.R;
+
+public class OutputPortfolioA extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_output_portfolio_a);
+
+    }
+}
---
+++ app/src/main/java/com/project/eportfolio/student/outputportfolio/OutputPortfolioD.java
@@ -0,0 +1,16 @@
+package com.project.eportfolio.student.outputportfolio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.eportfolio.R;
+
+public class OutputPortfolioD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_output_portfolio_d);
+    }
+}
